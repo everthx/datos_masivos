8952f1d79dc605a9132712be69ff1e4e89a5f3f2
SalmaFabel <salma.hernandez16@tectijuana.edu.mx>
2020-10-02 15:26:11 -0700

    Update Session_02.scala

--- Session_02.scala
+++ Session_02.scala
@@ -1,8 +1,17 @@
 // Assessment 1/Practica 1
 //1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
+
+val a=17.27
+val radio= a/(2*3.1416)
+
 //2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+    
+
 //3. Dada la variable bird = "tweet", utiliza interpolacion de string para
 //   imprimir "Estoy ecribiendo un tweet"
+
+var bird = "tweet"
+println ("Estoy ecribiendo un " + bird)
 
 //4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
 //   secuencia "Luke"
@@ -11,7 +20,7 @@ var mensaje = "Hola Luke yo soy tu padre!"
 mensaje.slice(5,9)
 
 //5. Cual es la diferencia entre value y una variable en scala?
-
+//la diferencia es que variable se puede modificar (es mutable) y value es inmutable, que no se puede modificar
 val valor = "Valor inmutabe/no puede ser modificado"
 var variable = "Variable mutable/si puede ser modificada"
 
