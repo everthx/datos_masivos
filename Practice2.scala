b4724b72fb8f04dbac4e2243b5bab7bd1e91ab64
everth <everth@virtual-Lubuntu.ipv6.infinitum.net.mx>
2020-10-06 18:23:21 -0700

    Unit_1: Answered points 5,6 and 7 for Practice2.scala

--- Practice2.scala
+++ Practice2.scala
@@ -3,8 +3,23 @@
 // 2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
 // 3. Traer los elementos de "lista" "verde", "amarillo", "azul"
 // 4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
+
 // 5. Cuales son los elementos unicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversion a conjuntos
+
+val lista = List(1,3,3,4,6,7,3,7)
+val elementosUnicos = lista.toSet
+
 // 6. Crea una mapa mutable llamado nombres que contenga los siguiente
 //     "Jose", 20, "Luis", 24, "Ana", 23, "Susana", "27"
+
+val mutableMap = collection.mutable.Map(("Jose", 20), ("Luis", 24), ("Ana", 23), ("Susana", "27"))
+
 // 6 a . Imprime todas la llaves del mapa
+
+mutableMapb get "Jose"
+mutableMapb get "Luis"
+mutableMapb get "Ana"
+mutableMapb get "Susana"
+
 // 7 b . Agrega el siguiente valor al mapa("Miguel", 23)
+mutableMap += ("Miguel" -> 23)
