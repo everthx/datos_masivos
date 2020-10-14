f45d497f877a861ebf3e6c4722f3b3cb6973921c
SalmaFabel <salma.hernandez16@tectijuana.edu.mx>
2020-10-13 17:27:16 -0700

    Practice 4 "Algorithm 1 Fibonacci"

--- Practices/Practice 4.scala
+++ Practices/Practice 4.scala
@@ -1,6 +1,19 @@
 //Practica 4 Implementar series Fibonacci
 
 //Algorithm 1
+import scala.math.sqrt
+
+def fibonacci(n: Int): BigInt =
+{
+if(n<2){
+return n
+}
+else{
+return fibonacci (n-1) + fibonacci (n-2)
+}
+}//scala> fibonacci(10) 
+ //res8: BigInt = 55
+
 
 //Algorithm 2
 
