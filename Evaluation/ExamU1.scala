3e2c994223bf22c152748b8c39ef4f064e19bdbd
everth <everth@virtual-Lubuntu.ipv6.infinitum.net.mx>
2020-10-24 21:26:26 -0700

    Unit_1: Corrected exam 1

--- Evaluation/ExamU1.scala
+++ Evaluation/ExamU1.scala
@@ -14,7 +14,8 @@ df.columns//res2: Array[String] = Array(Date, Open, High, Low, Close, Volume, Ad
 //¿Cómo es el esquema?
 df.printSchema ()
 //Imprime las primeras 5 columnas.
-df.head(5)
+df.head(5) //Imprime filas
+df.select($"Date",$"Open",$"High",$"Low",$"Close").show() //Imprime Columnas
 
 //Usa describe () para aprender sobre el DataFrame.
 df.describe().show()
@@ -23,14 +24,13 @@ df.describe().show()
 //acciones negociadas por un día. (Hint: Es una operación de columnas).
 val dfnew = df.withColumn("HV Ratio", df("High")/df("Volume")).show()
 
-dfnew.show()
-
 //¿Qué día tuvo el pico más alto en la columna “Close”?
 df.orderBy($"Close".desc).show(1)
 
 //Escribe con tus propias palabras en un comentario de tu código. ¿Cuál es el
 //significado de la columna Cerrar “Close”?
-//es el corte en la posicion de la bolsa
+
+/*Close is the average relation of exchange between High and Low that Netflix close don that day*/
 
 //¿Cuál es el máximo y mínimo de la columna “Volume”?
 df.select(max("Volume")).show()
@@ -44,13 +44,16 @@ df.select(min("Volume")).show()
 
 //¿Cuántos días fue la columna “Close” inferior a $ 600?
 df.filter($"Close"<600).count()
+
 //¿Qué porcentaje del tiempo fue la columna “High” mayor que $ 500?
+var hdf = df.filter($"High">500).count()*100/df.filter($"High">0).count()
 
 //¿Cuál es la correlación de Pearson entre columna “High” y la columna “Volumen”?
 df.stat.corr("High","Volume")
 
 //¿Cuál es el máximo de la columna “High” por año?
+df.groupBy(year(df("Date")).alias("Year")).max("High").sort(asc("Year")).show()
 
 //¿Cuál es el promedio de columna “Close” para cada mes del calendario?
-
+df.groupBy(month(df("Date")).alias("Months")).max("Close").sort(asc("Months")).show()
 
