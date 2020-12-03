4e8681fdea7be2a2e7577cfc9a2a8c81f268a543
Salma Fabel <salma.hernandez16@tectijuana.edu.mx>
2020-12-02 17:40:49 -0800

    update practice 8

--- Practices/Practice 8 Linear Support Vector Machine/Code.scala
+++ Practices/Practice 8 Linear Support Vector Machine/Code.scala
@@ -3,12 +3,15 @@
 //importing the Libraries
 import org.apache.spark.ml.classification.LinearSVC
 
-val spark = SparkSession.builder.appName("LinearSVCExample").getOrCreate()
+import org.apache.spark.sql.SparkSession
+
 //Load training data
 val training = spark.read.format("libsvm").load("C:/Users/salmi/OneDrive/Documentos/GitHub/datos_masivos/Practices/Practice 8 Linear Support Vector Machine/sample_libsvm_data.txt")
 
 val lsvc = new LinearSVC().setMaxIter(10).setRegParam(0.1)
 //Fit the model
-val lsvcModel = lsvc.fit( training )
+
+val lsvcModel = lsvc.fit(training)
+
 //Print the coefficients and intercept for linear svc
 println(s"Coefficients: ${lsvcModel.coefficients} Intercept: ${lsvcModel.intercept}")
