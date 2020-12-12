0b1b37ec29a6252c9f7dfc88aa0e2b42c398a5f4
Salma Fabel <salma.hernandez16@tectijuana.edu.mx>
2020-12-12 14:27:20 -0800

    Update ExamU2.scala

--- Evaluation/ExamU2.scala
+++ Evaluation/ExamU2.scala
@@ -1,19 +1,13 @@
 //Exam Unit 2
 
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.sql.SparkSession
 
 val spark = SparkSession.builder().getOrCreate()
 val dataframe = spark.read.option("header", "true").option("inferSchema","true")csv("iris.csv")
-
-var df = dataframe.distinct()
-
-df = df.withColumn("speciesNumber", when($"species"==="setosa","1").when(col("species")==="virginica","2").otherwise("3"))
-
-import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-
-
-dataframe.columns//Array[String] = Array(sepal_length, sepal_width, petal_length, petal_width, species)
 
 dataframe.printSchema()
  //|-- sepal_length: double (nullable = true)
@@ -35,9 +29,46 @@ dataframe.show(5)
 //+------------+-----------+------------+-----------+-------+
 //only showing top 5 rows
 
-dataframe.describe().show()
+val df = dataframe.withColumn("species", when($"species"==="setosa","1").when(col("species")==="virginica","2").otherwise("3").cast("int"))
 
-val splits = dataframe.randomSplit(Array(0.7, 0.3), seed = 1234L)
+df.columns//Array[String] = Array(sepal_length, sepal_width, petal_length, petal_width, species)
+
+df.printSchema()
+// |-- sepal_length: double (nullable = true)
+//|-- sepal_width: double (nullable = true)
+//|-- petal_length: double (nullable = true)
+//|-- petal_width: double (nullable = true)
+//|-- species: integer (nullable = true)
+
+df.show(5)
+
+//+------------+-----------+------------+-----------+-------+
+//|sepal_length|sepal_width|petal_length|petal_width|species|
+//+------------+-----------+------------+-----------+-------+
+//|         5.1|        3.5|         1.4|        0.2|      1|
+//|         4.9|        3.0|         1.4|        0.2|      1|
+//|         4.7|        3.2|         1.3|        0.2|      1|
+//|         4.6|        3.1|         1.5|        0.2|      1|
+//|         5.0|        3.6|         1.4|        0.2|      1|
+//+------------+-----------+------------+-----------+-------+
+//only showing top 5 rows
+
+
+df.describe().show()
+
+//Practice 1  and 2
+val assembler = (new VectorAssembler().setInputCols(Array("sepal_length", "sepal_width","petal_length", "petal_width")).setOutputCol("features"))
+val  features = assembler.transform(df)
+features.show
+//+------------+-----------+------------+-----------+-------+-----------------+
+//|sepal_length|sepal_width|petal_length|petal_width|species|         features|
+//+------------+-----------+------------+-----------+-------+-----------------+
+//|         5.1|        3.5|         1.4|        0.2|      1|[5.1,3.5,1.4,0.2]|
+//|         4.9|        3.0|         1.4|        0.2|      1|[4.9,3.0,1.4,0.2]|
+//|         4.7|        3.2|         1.3|        0.2|      1|[4.7,3.2,1.3,0.2]|
+//|         4.6|        3.1|         1.5|        0.2|      1|[4.6,3.1,1.5,0.2]|
+
+val splits = assembler.randomSplit(Array(0.7, 0.3), seed = 1234L)
 val train = splits(0)
 val test = splits(1)
 
@@ -45,13 +76,7 @@ val layers = Array[Int](4, 5, 4, 3)
 
 val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(100)
 
-//error Practice 1  and 2
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.linalg.Vectors
-
-val assembler = (new VectorAssembler().setInputCols(Array("", "")).setOutputCol("features"))
-
-
+//error 
 val model = trainer.fit(train)
 
 val result = model.transform(test)
