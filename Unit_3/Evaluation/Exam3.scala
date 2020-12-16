50479f5bc1cebf53bf47b6e3c6a85bfe134a5b98
Salma Fabel <salma.hernandez16@tectijuana.edu.mx>
2020-12-16 15:12:28 -0800

    Finished ExamU3

--- Unit_3/Evaluation/Exam3.scala
+++ Unit_3/Evaluation/Exam3.scala
@@ -55,13 +55,15 @@ val kmeans = new KMeans().setK(3).setSeed(1L)
 val model = kmeans.fit(output)
 
 //Evaluate the groups using Within Set Sum of Squared Errors WSSSE and print the centroids
-val WSSE = model.computeCost(output)//WSSE: Double = 8.095172370767671E10
-println(s"Within set sum of Squared Errors = $WSSE")//Within set sum of Squared Errors = 8.095172370767671E10
+val WSSE = model.computeCost(output)
+println(s"Within set sum of Squared Errors = $WSSE")
+//Within set sum of Squared Errors = 8.095172370767671E10
 
 //print the centroids
 println("Cluster Centers: ")
 model.clusterCenters.foreach(println)
 
+//Cluster Centers: 
 //[7993.574780058651,4196.803519061584,5837.4926686217,2546.624633431085,2016.2873900293255,1151.4193548387098]
 //[9928.18918918919,21513.081081081084,30993.486486486487,2960.4324324324325,13996.594594594595,3772.3243243243246]
 //[35273.854838709674,5213.919354838709,5826.096774193548,6027.6612903225805,1006.9193548387096,2237.6290322580644]
