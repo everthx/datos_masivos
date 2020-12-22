ad26170ed257392c78dd521ae83e2c46e0e50c72
Salma Fabel <salma.hernandez16@tectijuana.edu.mx>
2020-12-21 17:57:18 -0800

    Update Final Proyect

---
+++ finalProject/FinalProyect.scala
@@ -0,0 +1,9 @@
+// Final Proyect
+//Hernández Negrete Salma Fabel - 16212354
+//Luna Fuentes Fernando - 16210975
+
+//1.- Objective: Comparison of the performance following machine learning algorithms
+// - SVM
+// - Decision Three
+// - Logistic regression
+// - Multilayer Perceptron
