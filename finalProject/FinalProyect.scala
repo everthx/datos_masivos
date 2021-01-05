85b33221fa54e9032251f79c33af9f3afb48a9cd
Salma Fabel <salma.hernandez16@tectijuana.edu.mx>
2021-01-04 19:48:27 -0800

    finished final proyect "multilayer perceptron"

--- finalProject/FinalProyect.scala
+++ finalProject/FinalProyect.scala
@@ -253,3 +253,72 @@ println(metrics.confusionMatrix)
 //res3: Double = 0.891158935582376
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Multilayer Perceptron<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.StringIndexer 
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
+
+//Minimize errors
+import org.apache.log4j._
+Logger.getLogger("org").setLevel(Level.ERROR)
+
+//Start a simple Spark Session
+import org.apache.spark.sql.SparkSession
+val spark = SparkSession.builder().getOrCreate()
+
+//Our Dataset is loaded into a Dataframe
+val df= spark.read.option("header","true").option("inferSchema","true").option("delimiter",";").format("csv").load("bank-full.csv")
+
+//
+val stringindexer = new StringIndexer().setInputCol("y").setOutputCol("label")
+val df = stringindexer.fit(dataframe).transform(dataframe)
+
+val assembler = new VectorAssembler().setInputCols(Array("balance","day","duration","pdays","previous")).setOutputCol("features")
+
+val output = assembler.transform(df).select($"label", $"features")
+
+val split = output.randomSplit(Array(0.6, 0.4), seed = 1234L)
+val train = splits(0)
+val test = splits(1)
+
+val layers = Array[Int](5, 4, 1, 2)
+output.show()
+
+val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(100)
+
+val model = trainer.fit(train)
+
+val result = model.transform(test)
+
+val predictionAndLabels = result.select("prediction", "label")
+predictionAndLabels.show
+//+----------+-----+
+//|prediction|label|
+//+----------+-----+
+//|       0.0|  1.0|
+//|       0.0|  0.0|
+//|       0.0|  1.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  1.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//|       0.0|  0.0|
+//+----------+-----+
+val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
+
+println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
+//Test set accuracy = 0.8829233550649208
